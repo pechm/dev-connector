0a447e8c51fb998b91fa404f90841cd5058de004
Peter Chmeruk <peter.chmeruk@gmail.com>
2019-06-07 16:14:16 +0300

    User registration was added

--- config/db.js
+++ config/db.js
@@ -5,7 +5,8 @@ const db = config.get('mongoURL');
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useCreateIndex: true
     });
 
     console.log('MongoDB Connected ...');
