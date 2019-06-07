a55a3e5e6b3d174cc75737e2b35dff4305811a44
Peter Chmeruk <peter.chmeruk@gmail.com>
2019-06-07 17:31:29 +0300

    JWT token creation was added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const router = express.Router();
 const {
   check,
@@ -69,7 +71,23 @@ router.post(
 
       await user.save();
 
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'), {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({
+            token
+          });
+        });
     } catch (err) {
       console.error(err.message);
       return res.status(500).send('Server error');
