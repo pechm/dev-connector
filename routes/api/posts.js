dc1084b81a3610ff19f7e036009e8809ea55fff0
Peter Chmeruk <peter.chmeruk@gmail.com>
2019-06-12 13:20:40 +0300

    Add delete post comment was added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -95,9 +95,7 @@ router.get('/:id', auth, async (req, res) => {
 // @access Pirvate
 router.delete('/:id', auth, async (req, res) => {
   try {
-    console.log(req.params.id);
     const post = await Post.findById(req.params.id);
-    console.log(post);
     if (!post) {
       return res.status(400).json({
         msg: 'User not authorized'
@@ -170,6 +168,91 @@ router.put('/unlike/:id', auth, async (req, res) => {
     await post.save();
 
     res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  POST api/posts/comment
+// @desc   Create post comment
+// @access Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required').not().isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array()
+      });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  });
+
+// @route  DELETE api/posts/comment/:id/:comment_id
+// @desc   Delete post comment
+// @access Pirvate
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        msg: 'Post not found'
+      });
+    }
+
+    // Get comment from post
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+    if (!comment) {
+      return res.status(404).json({
+        msg: 'Comment not found'
+      });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({
+        msg: 'User not authorized'
+      });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments.map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    // deete comments
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
