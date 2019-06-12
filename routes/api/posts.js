a6550f3b0ee1c8c78305dca14f01fd4131975ce7
Peter Chmeruk <peter.chmeruk@gmail.com>
2019-06-12 12:12:20 +0300

    Get delete post was added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -50,4 +50,69 @@ router.post(
     }
   });
 
+// @route  GET api/posts
+// @desc   Get all posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({
+      date: -1
+    });
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  GET api/posts/:id
+// @desc   Get post by ID
+// @access Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({
+        msg: 'Post not found'
+      });
+    }
+
+    res.json(post);
+  } catch (err) {
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({
+        msg: 'Post not found'
+      });
+    }
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route  DELETE api/posts
+// @desc   Delete post
+// @access Pirvate
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const post = await Post.findById(req.params.id);
+    console.log(post);
+    if (!post) {
+      return res.status(400).json({
+        msg: 'User not authorized'
+      });
+    }
+
+    await post.remove();
+
+    res.json({
+      msg: 'Post deleted'
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
